9b5b5d747ea864b9227c916bc61d06893e216543
Roshan Gupta <roshangupta@Roshans-MacBook-Air.local>
2021-01-14 19:49:02 +0530

    uc5:Added function to create node

---
+++ MyNode.java
@@ -0,0 +1,26 @@
+public class MyNode<T> {
+    private MyNode next;
+    private T key;
+
+    public MyNode(T key) {
+        this.key = null;
+        this.next = null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+}
